6c436d30b5e259814ab064a1a3693ba6ef28b0a2
mecarm <melecarmine99@gmail.com>
2023-01-27 14:27:11 +0100

    fix / con \

--- app/Tag.php
+++ app/Tag.php
@@ -9,6 +9,6 @@ class Tag extends Model
     public function posts(){
 
         //I tag possono essere associati a più post
-        return $this->belongsToMany('App/Post');
+        return $this->belongsToMany('App\Post');
     }
 }
