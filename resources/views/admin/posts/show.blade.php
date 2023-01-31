180a571b7499b4c95ee66d35d3e326be0decb1e5
mecarm <melecarmine99@gmail.com>
2023-01-31 16:45:30 +0100

    stampata a schermo img

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <div class="text-center">
         <h1>{{$singolo_post->title}}</h1>
+        <div class="w-50 m-auto">
+            <img class="img-fluid" src="{{ asset("storage/$singolo_post->cover") }}" alt="">
+        </div>
         <p>{{$singolo_post->body}}</p>
     </div>
 @endsection
