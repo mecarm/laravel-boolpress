853765c61a5b5a0c0888eb0c9ee47914f3cdcc11
mecarm <melecarmine99@gmail.com>
2023-01-31 22:36:09 +0100

    edit della mail

--- resources/views/mails/posts/createPost.blade.php
+++ resources/views/mails/posts/createPost.blade.php
@@ -5,8 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mail</title>
+    <style>
+        body{
+            background-color: lightgoldenrodyellow;
+            color: rgb(39, 37, 37)
+        }
+        h1{
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>ciao</h1>
+    <h1>Questa mail è stata generata alla creazione del post</h1>
 </body>
 </html>
