74a0d803d74410579759ebe872bf206a23acd7eb
mecarm <melecarmine99@gmail.com>
2023-02-02 15:04:14 +0100

    creo card per la show

---
+++ resources/js/components/pages/posts/PostCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        {{ post.title }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PostCard',
+    components: {
+
+    },
+    data(){
+        return{
+            post: null,
+        }
+    },
+    mounted() {
+        this.getPost();
+    },
+    methods: {
+        getPost(){
+            axios.get('http://127.0.0.1:8000/api/post/' + this.$forceUpdate.params.id)
+                .then( res => {
+                    console.log(res.data);
+                    this.post = res.data;
+                }).catch(err => {
+                    console.log(err);
+                })
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/pages/posts/PostShow.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Pagina Singolo Post: </h1>
+        <!-- Componente post card -->
+        <PostCard/>
+    </div>
+</template>
+
+<script>
+import PostCard from './PostCard.vue'
+export default {
+    name: 'PostShow',
+    components: {
+        PostCard
+    },
+    data() { 
+
+    }
+}
+</script>
+
+<style>
+
+</style>
